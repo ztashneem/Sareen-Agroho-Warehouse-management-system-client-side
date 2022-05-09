4b37b046c11bfded87947f5eed5edc2b8c1b887e
ztashneem <z.tashneem12@gmail.com>
2022-05-09 20:58:42 +0600

    toast container

--- src/App.js
+++ src/App.js
@@ -7,14 +7,19 @@ import ManageItems from './Pages/ManageItmes/ManageItems';
 import AddItem from './Pages/ManageItmes/AddItem/AddItem';
 import Signin from './Pages/SigninPage/Signin/Signin';
 import Signup from './Pages/SigninPage/Signup/Signup';
+import RequireAuth from './Pages/SigninPage/RequireAuth/RequireAuth';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import MyItems from './Pages/ManageItmes/MyItems/MyItems';
+import Footer from './SharePage/Footer/Footer';
 
 function App() {
   return (
     <div className="">
       <Headers></Headers>
- 
+
       <Routes>
-      <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/manage' element={<ManageItems></ManageItems>}></Route>
         <Route path='/add' element={<AddItem></AddItem>}></Route>
@@ -23,8 +28,8 @@ function App() {
         <Route path='myitems' element={<RequireAuth><MyItems></MyItems></RequireAuth>}></Route>
         <Route path='signup' element={<Signup></Signup>}></Route>
       </Routes>
-     
-     
+      <Footer></Footer>
+      <ToastContainer></ToastContainer>
     </div>
   );
 }
