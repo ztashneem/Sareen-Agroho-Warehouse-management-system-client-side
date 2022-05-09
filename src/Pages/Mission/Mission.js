2cec0489158a273c1be2dc4096b689b7f3107b13
ztashneem <z.tashneem12@gmail.com>
2022-05-09 20:34:30 +0600

    our mission

---
+++ src/Pages/Mission/Mission.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Mission = () => {
+    return (
+        <div className='bg-gray-800 flex flex-col justify-center items-center h-[280px]'>
+            <p className=' text-white mb-10 text-xl'>OUR SUCCESS</p>
+            <h3 className='text-4xl whitespace-pre-line text-center text-white mb-4'>“Being perfectly well-dressed gives one a tranquillity <br /> that no religion can bestow”</h3>
+            {/* <h3 className='text-4xl whitespace-pre-line text-white'></h3> */}
+        </div>
+    );
+};
+
+export default Mission;
